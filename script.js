f4934b28f031aa1b9e3371fec9679debd6b4596c
reganmariadass <reganmariadass@gmail.com>
2025-04-27 20:44:46 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+const contractAddress = "YOUR_CONTRACT_ADDRESS_HERE";
+const contractABI = [ /* paste your ABI here */ ];
+
+let provider;
+let signer;
+let contract;
+
+async function connectWallet() {
+    if (window.ethereum) {
+        provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        signer = provider.getSigner();
+        const address = await signer.getAddress();
+        document.getElementById("walletAddress").innerText = "Connected: " + address;
+        contract = new ethers.Contract(contractAddress, contractABI, signer);
+    } else {
+        alert("Please install MetaMask!");
+    }
+}
+
+async function joinDAO() {
+    const tx = await contract.joinDAO();
+    await tx.wait();
+    alert("Joined DAO!");
+}
+
+async function createProposal() {
+    const desc = document.getElementById("proposalDesc").value;
+    const amount = ethers.utils.parseEther(document.getElementById("proposalAmount").value);
+    const tx = await contract.createProposal(desc, amount);
+    await tx.wait();
+    alert("Proposal Created!");
+}
+
+async function voteProposal() {
+    const id = document.getElementById("voteProposalId").value;
+    const tx = await contract.voteOnProposal(id);
+    await tx.wait();
+    alert("Voted Successfully!");
+}
+
+async function fundDAO() {
+    const value = ethers.utils.parseEther("0.1");
+    const tx = await signer.sendTransaction({
+        to: contractAddress,
+        value: value
+    });
+    await tx.wait();
+    alert("DAO Funded!");
+}
